0c2dea87ba4e1025939933a986e36953e983bab5
Kyle <mbio.kyle@gmail.com>
2013-12-04 14:42:53 -0600

    fixed GetOptions and regex

--- perl/multi_run.pl
+++ perl/multi_run.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
-use GetOpt::Long;
+use Getopt::Long;
 use threads;
 
 my $read_dir;
@@ -9,21 +9,24 @@ my $map_base;
 my $matched = 0;
 
 GetOptions ("read_dir=s" => \$read_dir,
-			"map_base=S" => \$map_base,
-			 "matched=i" => \$matched) or die("malformed command line args \n";
+			"map_base=s" => \$map_base,
+			 "matched=i" => \$matched) or die("malformed command line args \n");
 
 opendir DIR, $read_dir;
-my @read_files = grep { /^\./ } readdir(DIR);
+my @read_files = grep { !/^\./ } readdir(DIR);
 closedir DIR;
 
 my $match_arg = "-U" unless $matched;
 
-for my $read_file (@files)
+for my $read_file (@read_files)
 {	
-	$read_file =~ m/(.*)\.fastq$/;
-	my $output = $1;
-	my $results = `bowtie2 -X $map_base $read_dir/$read_file -S $output`;
-	print "For $read_file: \n";
-	print $results."\n";
+
+	if($read_file =~ m/^(.*)\.fastq$/)
+	{
+		my $output = $1.".sam";
+		my $results = "bowtie2 -t --no-unal -x $map_base $read_dir$read_file -S $output";
+		print "For $read_file: \n";
+		print $results."\n";
+	}
 }
 
