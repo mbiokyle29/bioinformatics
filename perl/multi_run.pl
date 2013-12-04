ca93b33352e42cf42caf596639e5d422f5010427
Kyle <mbio.kyle@gmail.com>
2013-12-04 15:19:33 -0600

    fullpath stuff

--- perl/multi_run.pl
+++ perl/multi_run.pl
@@ -3,6 +3,7 @@ use warnings;
 use strict;
 use Getopt::Long;
 use threads;
+use Cwd qw(abs_path);
 
 my $read_dir;
 my $map_base;
@@ -12,7 +13,14 @@ GetOptions ("read_dir=s" => \$read_dir,
 			"map_base=s" => \$map_base,
 			 "matched=i" => \$matched) or die("malformed command line args \n");
 
-opendir DIR, $read_dir;
+my $fp_read = abs_path($read_dir);
+`which bowtie2` =~ m/((^.*\/)[^\/]+$)/;
+my $fp_bowtie2 = $1;
+
+print $fp_read."\n";
+print $fp_bowtie2."\n";
+
+opendir DIR, $fp_read;
 my @read_files = grep { !/^\./ } readdir(DIR);
 closedir DIR;
 
@@ -24,7 +32,7 @@ for my $read_file (@read_files)
 	if($read_file =~ m/^(.*)\.fastq$/)
 	{
 		my $output = $1.".sam";
-		my $results = "bowtie2 -t --no-unal -x $map_base $read_dir$read_file -S $output";
+		my $results = "bowtie2 -t --no-unal -x $map_base $fp_read$read_file -S $output";
 		print "For $read_file: \n";
 		print $results."\n";
 	}
