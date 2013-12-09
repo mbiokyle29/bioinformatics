5dd750230298bb3961dc9e895eda805261d6675e
Kyle <mbio.kyle@gmail.com>
2013-12-08 18:20:40 -0600

    added an osx check for processor part, more paths and wrapped sam to bam in subroutine

--- perl/multi_run.pl
+++ perl/multi_run.pl
@@ -9,11 +9,26 @@ use warnings;
 use strict;
 use Getopt::Long;
 use threads;
-use feature 'say';
+use feature qw(say);
+use Switch;
 
 my $read_dir;
 my $map_base;
 my $matched = 0;
+
+# Figure out the number of cores to run on (total on machine - 1)
+my $cores;
+my $os = `uname -s`;
+chomp($os);
+
+switch($os)
+{
+	case "Linux" { $cores = `nproc`; }
+	case "Darwin" { $cores = `sysctl -n hw.ncpu`; }
+	else die "Are you on windows? \n";
+}
+
+chomp($cores);
 
 # $genoms{'genomeX'} = (count of that genome in alignment)
 our %genomes;
@@ -33,10 +48,7 @@ my $fp_results = $fp."results/";
 my $fp_read = $fp."reads/".$read_dir."/";
 my $fp_maps = $fp."maps/";
 
-# Figure out the number of cores to run on (total on machine - 1)
-# for linux my $cores = `nproc` - 1; 
-my $cores = `sysctl -n hw.ncpu`;
-chomp($cores);
+
 
 # Grab all the files, ignore . and ..
 opendir DIR, $fp_read;
@@ -51,23 +63,26 @@ for my $read_file (@read_files)
 	my $read_count = 0;
 	if($read_file =~ m/^(.*)\.fastq$/)
 	{
-		my $sam = $1.".sam";
+		my $base_name = $1;		
+		my $fp_sam = $fp_results.$1.".sam";
 		my $results = "bowtie2 -p $cores -t --no-unal -x $fp_maps$map_base $fp_read$read_file -S $fp_results$sam";
 		my $bowtie_output = `$results`;
 		say $bowtie_output;
+		
+		# Run Sam->Bam conversion
+		&sam_to_bam($fp_results.$base_name);
 
+		# If aligning to EBV no need to run chromosome stats
 		next if($map_base eq "EBV");
+		
 		# Output file and alignment counter
 		my $ts = time();
-		my $sam_stat = $fp_results.$sam.".$ts.stat";
+		my $sam_stat = $fp_$sam.".$ts.stat";
 		my $align_count = 0;
 		
-		my $fp_output = $fp_results.$sam;
-		
-		
 		# Read from one, write stats into other
-		open  SAM, '<', $fp_output;
-		open  STAT, '>', $sam_stat;
+		open  SAM, '<', $fp_sam;
+		open  STAT, '>', $fp_stat;
 		
 		# read in one line at a time and process
 		while (<SAM>)
@@ -96,6 +111,19 @@ for my $read_file (@read_files)
 		say STAT "output from bowtie2: ";
 		say STAT "$bowtie_output";
 		close STAT;
+	}
+	
+	# Takes filename and full path, makes bam files
+	# TODO should prolly handle errors and return codes
+	sub sam_to_bam 
+	{
+		my $base_name = shift;
+		my $sam = $base_name.".sam";
+		my $bam = $base_name.".bam";
+		my $sorted = $base_name.".sorted";
+		`samtools view -S -b -o $bam $sam`;
+		`samtools sort $bam $sorted`;
+		`samtools index $bam`;
 	}
 }
 
