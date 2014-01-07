38d847ef1ae0526519818623e8fabeda3fbbbfc1
Kyle <mbio.kyle@gmail.com>
2014-01-07 13:59:54 -0600

    stop reinventing the wheel gonna move to GATK

--- perl/calc_coverage.pl
+++ perl/calc_coverage.pl
@@ -10,69 +10,30 @@
 use warnings;
 use strict;
 use feature qw(say);
+use Data::Dumper;
 
 # Directory with sam files
 my $read_dir = shift;
-# Hash to build
+
+# Array to build
+## NOTE THIS IS HARD CODED
+my $total = 171823;
 my %base_counts;
+
+for(my $i = 1; $i <= $total; $i++)
+{
+	$base_counts{$i} = 0;
+}
+
 # Name of chromosome in SAMs, found during processing
 my $chromosome;
 
 # Get only the .sam files
 opendir DIR, $read_dir;
-my @align_files = grep { /.sam$/ } readdir(DIR);
+my @align_files = grep { /.bam$/ } readdir(DIR);
 closedir DIR;
 
 foreach my $file (@align_files)
 {
-	open SAM, '<', $read_dir.$file;
-	while(<SAM>)
-	{
-		next if $_ =~ m/^@/;
-		my @line = split(/\t/,$_);
-		
-		# Get the name of the chromosome once!
-		unless($chromosome)
-		{
-			$chromosome = $line[2];
-		}
-		
-		# Get the necessary fields
-		my $start_pos = $line[3];
-		my $read = $line[9];
-		chomp($read);
-		
-		# Add length to start pos, count back down to start pos, incrementing
-		my $length = length($read) - 1;
-		my $counter = $start_pos + $length;
-		while($counter >= $start_pos)
-		{
-			$base_counts{$counter}++;
-			$counter--;
-		}
-	}
+	my $
 }
-
-# Set up WIG file
-my $ts = time();
-my $wigfile = $read_dir.$chromosome.$ts."-wiggle.wig";
-open WIG, '>', $wigfile;
-say WIG "variableStep chrom=$chromosome";
-say "Chromosome: $chromosome";
-say "Poisition   # of reads";
-
-# Hard coded percent coverage
-my $total = 171823;
-my $mapped = keys(%base_counts);
-my $percent = ($mapped/$total)*100;
-
-# Do both wig and output in same loop
-foreach my $key (sort {$a<=>$b} keys(%base_counts))
-{
-	# Print to stdout
-	say "$key            $base_counts{$key}";
-	# Build wig file
-	say WIG "$key $base_counts{$key}";
-}
-close WIG;
-say "Percent mapped = % $percent";
