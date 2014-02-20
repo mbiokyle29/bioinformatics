20dff7759aa0afc3f4c4b15b67ca32effb247b7f
Kyle Mc <mbio.kyle@gmail.com>
2014-02-20 17:05:56 -0600

    sort of implemented, also amde a big mess

--- perl/cigar_mapper.pl
+++ perl/cigar_mapper.pl
@@ -4,7 +4,8 @@ use warnings;
 use strict;
 use Getopt::Long;
 use File::Slurp;
-use feature qw(say);
+use Data::Dumper;
+use feature qw(say switch);
 
 my ($start_pos, $end_pos, $sam);
 
@@ -50,43 +51,27 @@ foreach my $line (@lines)
 	my @fields = split("\t", $line);
 
 	# Columns are in the array, get the stuff we want
-	my @seq = split(//, $fields[9]); # String of the SEQ	
+	my $seq_string = $fields[9];
+	my @seq = split(//, $seq_string); # String of the SEQ	
 	my $read_length = length($fields[9]); #length of SEQ	
 	my $read_start = $fields[3]; # Starting position POS    
 	my $read_end =  ($read_start+$read_length)-1; # Ending position (POS+len(SEQ)-1)
 	
 	# Parse cigar string to calculate actual alignment length
 	# Also do some basic stats/qual
-	my $cigar_length = 0;	
 	my $cigar_string = uc($fields[5]); # upper case it for easy-mode
-	my $soft_count = 0; # Keep track of soft clipped
+	my @cigar_chunks;
 
-	# CIGAR string has chunks of |numbers_oneletter|
-	# dump all into array and then calc length
-	# TODO only worrying about D,M,I,S tags
-	my @cigar_chunks;	
-	while($cigar_string =~ m/(\d+[DMIS])/g)
+	## TODO IGNORE THE SOFT CLIPPED AND EVERYHTING ELSE
+	## TODO HUGE SOURCE OF ERROR BAD MUST FIX
+	while($cigar_string =~ m/(\d+[DMI])/g)
 	{
 		push(@cigar_chunks, $1);
-	}	
-	
-	# Process each "chunk" of the cigar string
-	foreach my $chunk (@cigar_chunks)
-	{		
-		# added the ++ b/c index starts at 0
-		if((index($chunk, "D")+ 1) || (index($chunk, "M")+ 1))
-		{			
-			# D's and M's contribute to the "cigar" length			
-			$chunk =~ m/(\d+)/;
-			$cigar_length += $1;
-		}
-		if(index($chunk, "S")+1)
-		{
-			# S's contribute to the soft clipped count
-			$chunk =~ m/(\d+)/;
-			$soft_count += $1;
-		}
 	}
+
+	my $cigar_stack = &build_cigar_stack(\@cigar_chunks);
+	my $cigar_length = &calc_cigar_length(\@cigar_chunks);
+	my $soft_count = &calc_soft_count(\@cigar_chunks);
 	
 	# results	
 	my $soft_to_total = ($soft_count/$read_length);
@@ -111,12 +96,41 @@ foreach my $line (@lines)
 
 	# If the input start point is more then the read end
 	next if($start_pos > $cigar_end);
+	
+	# Build alignemt hash
+	my $alignment_ref = &build_alignment_hash($seq_string, $read_start, $cigar_stack);
+	my %alignment = %$alignment_ref;
 
 	# Valid alignement that overlaps the range	
 	say $p_out "Match!, For input range: $start_pos - $end_pos";
+	
+	my $range = $start_pos;
+	while($range <= $end_pos)
+	{
+		say $p_out "$range: $alignment{$range}";
+		$range++;
+	}
+	
 	say $p_out "The read starting from $cigar_start to $cigar_end with string $cigar_string";
 	say $p_out "(the calculated cigar length was: $cigar_length";
-	say $p_out "\n $line \n";
+	say $p_out " \n $line \n";
+	
+	#say $p_out "The calculated alignment sequnce is:";
+	#my $count = 0;
+	#foreach my $key (sort( {$a <=> $b} keys(%alignment)))
+	#{
+	#	if($count < 10)
+	#	{
+	#		print $p_out "$key: $alignment{$key} ";
+	#		$count++
+	#	}
+	#	else 
+	#	{ 
+	#		say $p_out "$key: $alignment{$key}";
+	#		$count = 0 
+	#	}
+	#}
+	say $p_out "\n";
 }
 
 # Close up shop
@@ -124,3 +138,88 @@ say $p_out "total good reads = $passed_reads out of total $total_reads";
 say $f_out "total bad reads = $failed_reads out of total $total_reads";
 close $p_out;
 close $f_out;
+
+
+sub build_cigar_stack
+{
+	my $cigar = shift;
+	my @cigar_chunks = @$cigar;
+	my $cigar_stack;
+	foreach my $chunk (@cigar_chunks)
+	{
+		my $code = chop($chunk);
+		my $push = $code x $chunk;
+		$cigar_stack.= $push;
+	}
+	return $cigar_stack;
+}
+
+sub calc_cigar_length
+{
+	my $cigar = shift;
+	my @cigar_chunks = @$cigar;
+	my $cigar_length = 0;
+
+	foreach my $chunk (@cigar_chunks)
+	{				
+		if(chop($chunk) =~ m/[MD]/)		
+		{			
+			$chunk =~ m/(\d+)/;
+			$cigar_length += $chunk;
+		}
+	}
+	return $cigar_length;
+}
+
+sub calc_soft_count
+{
+	my $cigar = shift;
+	my @cigar_chunks = @$cigar;
+	my $soft_count = 0;
+
+	foreach my $chunk (@cigar_chunks)
+	{				
+		if(chop($chunk) eq "S")		
+		{			
+			$chunk =~ m/(\d+)/;
+			$soft_count += $chunk;
+		}
+	}
+	return $soft_count;
+}
+
+# Takes:
+#	a raw read sequence (scalar)
+#	a start position (the ref base position where the alignment started) (scalar)
+#	a special CIGAR 'stack' *see &build_cigar_stack () (scalar)
+sub build_alignment_hash
+{
+	my @arr_seq = split(//,shift);
+	my $ref_pointer = shift;
+	my @cigar_stack = split(//,shift);
+	my $seq_pointer = 0;
+	my %alignment;
+
+	foreach my $cigar (@cigar_stack)
+	{
+		given($cigar)
+		{
+			when("M")
+			{
+				$alignment{$ref_pointer} = $arr_seq[$seq_pointer];
+				$ref_pointer++;
+				$seq_pointer++;
+			}
+			when("D")
+			{
+				$alignment{$ref_pointer} = "X";
+				$ref_pointer++;
+			}
+			when("I")
+			{
+				$ref_pointer++;
+			}
+		}
+	}
+	return \%alignment;
+}
