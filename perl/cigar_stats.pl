641216091508df9138662347a1b3d8e1afa35160
Kyle Mc <mbio.kyle@gmail.com>
2014-02-21 11:23:09 -0600

    split cigar into two stats and mapper, mapper should be working still need to do matrix

--- perl/cigar_stats.pl
+++ perl/cigar_stats.pl
@@ -7,21 +7,19 @@ use File::Slurp;
 use Data::Dumper;
 use feature qw(say switch);
 
-my ($start_pos, $end_pos, $sam, $reference);
+my ($start_pos, $end_pos, $sam);
 
 GetOptions (
 	"s=i" => \$start_pos,
 	"e=i" => \$end_pos,
 	"r=s" => \$sam,
-	"m=s" => \$reference
 );
 
 # CHECK ARGS
 # If not all three entered die
-unless ($sam && $reference)
+unless ($start_pos && $end_pos && $sam)
 {
-	say "-r flag and -m flag are required!";
-	die "read_finder.pl -s starting_position -e ending_position -r sam_file -m reference_genome";
+	die "read_finder.pl -s starting_position -e ending_position -r sam_file";
 }
 
 # if the ending position is less then the start position
@@ -31,22 +29,25 @@ if($end_pos < $start_pos)
 	die "read_finder.pl -s starting_position -e ending_position -r sam_file";
 }
 
-# Get the reference genome and reads
-my @reference_file = read_file($reference);
-my @ref_seq = split(//, pop(@reference_file));
+# Set up output
+my $total_reads = 0;
+my $failed_reads = 0;
+my $passed_reads = 0;
+
+$sam =~ m/(.*).sam$/;
+my $passed = $1.".passed";
+my $failed = $1.".failed";
+
+open my $p_out, ">", $passed;
+open my $f_out, ">", $failed;
+
+# Get reads
 my @lines = read_file($sam);
-
-# Get rid of EOF
-pop(@ref_seq);
-
-$sam =~ m/(.*)\.sam$/;
-my $output = $1.".cigar";
-open my $out, ">", $output;
-
 foreach my $line (@lines)
 {
 	# skip the junk at the start
 	next if ($line =~ m/^@/);
+	$total_reads++;
 	my @fields = split("\t", $line);
 
 	# Columns are in the array, get the stuff we want
@@ -68,12 +69,28 @@ foreach my $line (@lines)
 		push(@cigar_chunks, $1);
 	}
 
-	# Results
 	my $cigar_stack = &build_cigar_stack(\@cigar_chunks);
 	my $cigar_length = &calc_cigar_length(\@cigar_chunks);
+	my $soft_count = &calc_soft_count(\@cigar_chunks);
+	
+	# results	
+	my $soft_to_total = ($soft_count/$read_length);
 	my $cigar_start = $read_start;
 	my $cigar_end = ($read_start+$cigar_length-1);
 
+	# If read has high level of soft clipped, redirect to failed and report	
+	if($soft_to_total >= .5)
+	{
+		$failed_reads++;
+		say $f_out "$cigar_string had soft/total of: $soft_to_total";
+		say $f_out "for:";
+		say $f_out "$line";
+		next;
+	}
+	
+	# House keeping
+	$passed_reads++;	
+	
 	# If the input range ending point is less then the cigar/read start
 	next if($end_pos < $cigar_start); 
 
@@ -84,21 +101,44 @@ foreach my $line (@lines)
 	my $alignment_ref = &build_alignment_hash($seq_string, $read_start, $cigar_stack);
 	my %alignment = %$alignment_ref;
 
-	# Valid alignment that overlaps the range
-	say $out "Match!, For input range: $start_pos - $end_pos";	
-	say $out "The read starting from $cigar_start to $cigar_end";
-	say $out "(the calculated cigar length was: $cigar_length";
-	say $out "with string $cigar_string";
-	say $out "The calculated alignment sequnce is:";
+	# Valid alignement that overlaps the range	
+	say $p_out "Match!, For input range: $start_pos - $end_pos";
 	
-	my $count = $start_pos;
-	while($count <= $end_pos)
+	my $range = $start_pos;
+	while($range <= $end_pos)
 	{
-		say $out "$count";
-		say $out "$alignment{$count} --  $ref_seq[$count-1]";
-		$count++;
+		say $p_out "$range: $alignment{$range}";
+		$range++;
 	}
+	
+	say $p_out "The read starting from $cigar_start to $cigar_end with string $cigar_string";
+	say $p_out "(the calculated cigar length was: $cigar_length";
+	say $p_out " \n $line \n";
+	
+	#say $p_out "The calculated alignment sequnce is:";
+	#my $count = 0;
+	#foreach my $key (sort( {$a <=> $b} keys(%alignment)))
+	#{
+	#	if($count < 10)
+	#	{
+	#		print $p_out "$key: $alignment{$key} ";
+	#		$count++
+	#	}
+	#	else 
+	#	{ 
+	#		say $p_out "$key: $alignment{$key}";
+	#		$count = 0 
+	#	}
+	#}
+	say $p_out "\n";
 }
+
+# Close up shop
+say $p_out "total good reads = $passed_reads out of total $total_reads";
+say $f_out "total bad reads = $failed_reads out of total $total_reads";
+close $p_out;
+close $f_out;
+
 
 sub build_cigar_stack
 {
@@ -177,7 +217,7 @@ sub build_alignment_hash
 			}
 			when("I")
 			{
-				$seq_pointer++;
+				$ref_pointer++;
 			}
 		}
 	}
