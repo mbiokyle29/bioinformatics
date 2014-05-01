1e31c00f1e0fc80898b3675ab33d9b3df329269b
Kyle Mc <mbio.kyle@gmail.com>
2014-05-01 10:54:58 -0500

    one off for merging all the sets into a genome wide

---
+++ perl/mosaics/merge_bins.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+# Merge bins of genome files into one huge one to feed to mosaics
+# Takes a dir and a type (M, N, GC,)
+# Expects 'mosaics' file like file names
+use warnings;
+use strict;
+use Data::Printer;
+use feature qw|say|;
+use Getopt::Long;
+use File::Slurp;
+
+# Predef
+my ($dir, $type, $output);
+GetOptions(
+	"dir=s" => \$dir,
+	"type=s" => \$type,
+	"output=s" => \$output,
+);
+
+unless($type =~ m/GC|N|map/ && $dir)
+{
+	die "merge_bins.pl --dir DIR --type (GC|N|map) --output output";
+}
+
+my @files = grep {/$type/} read_dir($dir);
+p(@files);
+
+open my $outfile, ">", $dir.$output;
+
+foreach my $file (@files)
+{
+	$file =~ m/(^chr.+)(?=_$type)/;
+	my $chrID = $1;
+	say "$file generated id = $chrID";
+	my @data = read_file($file);
+	foreach my $dataline (@data)
+	{
+		chomp($dataline);
+		say $outfile "$chrID\t$dataline";
+	}
+}
+close $outfile;
